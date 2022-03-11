71f8206c2445c93a7ab4de30f849c52a48043701
Dener Sousa <90757341+denersousa@users.noreply.github.com>
2022-03-11 10:51:47 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {HttpClient, HttpHandler } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  photos: Object[] = [];
+
+  constructor(http: HttpClient){
+
+    http
+    .get<Object[]>('http://localhost:3000/flavio/photos')
+    .subscribe(photos => this.photos = photos);
+
+  }
+}
